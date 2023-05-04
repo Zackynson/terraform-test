baefafad56a79955c069747a97b50d155275f32d
zackynson <crys.chb@hotmail.com>
2023-05-04 16:14:54 -0300

    chore: api gtw + simple lambda

---
+++ main.tf
@@ -0,0 +1,80 @@
+resource "aws_api_gateway_rest_api" "api" {
+  name = "terraform-http-api"
+}
+
+data "archive_file" "example-zip" {
+  output_path = "${path.module}/example.zip"
+  source_file = "${path.module}/hello-world/index.js"
+  type        = "zip"
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename      = "example.zip"
+  function_name = "Example"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.handler"
+  runtime       = "nodejs16.x"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_lambda_permission" "permission_for_lambda" {
+  statement_id  = "AllowApiGatewayToInvokeLambda"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}*/*"
+}
+
+resource "aws_api_gateway_resource" "MyDemoResource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "mydemoresource"
+}
+
+resource "aws_api_gateway_method" "MyDemoMethod" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.MyDemoResource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+
+resource "aws_api_gateway_integration" "example" {
+  http_method             = aws_api_gateway_method.MyDemoMethod.http_method
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.MyDemoResource.id
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+# Lambda
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:us-east-1:566163553601:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.MyDemoMethod.http_method}${aws_api_gateway_resource.MyDemoResource.path}"
+}
